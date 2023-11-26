13008acd7dbad6af7d6834b39d3980936393e442
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2023-11-26 01:53:41 +0000

    checker errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -77,7 +77,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     user = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
     host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', "")
     conn = mysql.connector.connect(user=user,
                                    password=passwd,
                                    host=host,

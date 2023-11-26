21fe4234d131cdfbbfeffa6c5edb00ca8ff69466
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2023-11-26 01:49:45 +0000

    authentication

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -74,13 +74,14 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
+    host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
     conn = mysql.connector.connect(user=user,
                                    password=passwd,
                                    host=host,
+                                   port=3306,
                                    database=db_name)
     return conn
 
